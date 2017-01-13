c8991c611e497e683600338738a77dcd56350c39
Terence Highsmith <terence.highsmith@gmail.com>
2017-01-13 16:18:23 -0600

    added child component exercise for react

--- 4. Redux & React/1. only React/v03. child Component/src/index.js
+++ 4. Redux & React/1. only React/v03. child Component/src/index.js
@@ -1,24 +1,35 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-class Examples extends Component {
+class Counter extends Component {
     constructor(props){
         super(props)
-        this.state = {count: 0, sum: 3, a:1, b:2}
+        this.state = {count: 0}
         this.decrease = this.decrease.bind(this); // bind the decrease method to the example class
     }
     decrease(){
         this.setState({count: this.state.count - 1})
     }
-
-  render() {
+    render() {
     return (
         <div>
-            <h1>React</h1>
             <p>
                 Clicked: <span >{this.state.count}</span> times
                 <button onClick={this.decrease}>Decrease</button>
             </p>
+        </div>
+    )
+  }
+}
+
+class Sum extends Component {
+    constructor(props){
+        super(props)
+        this.state = {sum: 3, a:1, b:2}
+    }
+    render() {
+    return (
+        <div>
             <p>
                 <input value={this.state.a} onChange={(e)=>this.setState({a: e.target.value})}/>
                 + 
@@ -27,6 +38,18 @@ class Examples extends Component {
                 <span>{this.state.sum}</span>
                 <button onClick={()=> this.setState({sum: Number(this.state.a) + Number(this.state.b)})}>Sum</button>
             </p>
+        </div>
+    )
+  }
+}
+
+class Examples extends Component {
+  render() {
+    return (
+        <div>
+            <h1>React</h1>
+            <Counter/>
+            <Sum/>
         </div>
     )
   }
