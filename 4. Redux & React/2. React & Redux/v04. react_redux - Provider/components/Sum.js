2b31fd7ce250dc90f0d60596cf660def3a073d84
Terence Highsmith <terence.highsmith@gmail.com>
2017-02-13 17:29:47 -0600

    added new provider project folders and content

---
+++ 4. Redux & React/2. React & Redux/v04. react_redux - Provider/components/Sum.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getSum } from '../redux/actions/index'
+
+class Sum extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {a:1, b: 2}
+  }
+  render() {
+    const {state:{a,b}, props:{sum, onSum}} = this;
+    return (
+      <div>
+        <input value={a} onChange={(e)=>this.setState({a: e.target.value})}/> 
+        + <input value={b} onChange={(e)=>this.setState({b: e.target.value})}/>
+        = <span>{sum}</span>
+        <button onClick={() => 
+          onSum(a,b)}>
+          Sum</button>
+        <p/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  sum: state.sum
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onSum: (a,b) => {
+    dispatch(getSum(a,b))
+  }
+})
+
+const SumContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sum)
+
+export default SumContainer
